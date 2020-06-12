963af249eed746549105181aef4e2dfa499e451c
bhakti-automation <63325735+bhakti-automation@users.noreply.github.com>
2020-06-12 18:42:01 -0500

    added again

---
+++ MyAbstract/src/myabstract/Addthis.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myabstract;
+
+/**
+ *
+ * @author virad
+ */
+public class Addthis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("feature branch");
+        // TODO code application logic here
+    }
+    
+}
---
+++ MyAbstract/src/myabstract/NewlyAdded.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myabstract;
+
+/**
+ *
+ * @author virad
+ */
+public class NewlyAdded {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("add to new branch");
+        System.out.println("Try to add now");
+    }
+    
+}
